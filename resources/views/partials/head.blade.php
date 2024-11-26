a931d090c6fcdeca6294763bae8bfbbb2335de6f
hangnt <hangnt2@bsscommerce.com>
2024-11-26 23:05:05 +0700

    ISET-97:update file import font

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -7,7 +7,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
 <link rel="stylesheet" href="{{ asset('css/owl.theme.default.css') }}">
-
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <style>
     *,
     :before,
